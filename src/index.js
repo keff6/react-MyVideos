fd50e19f70e72db6b80e5e4446437f5eb61ca384
keff6 <kev.fallas@gmail.com>
2018-02-09 16:53:48 -0600

    Search Component implementation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SearchBar from './components/search-bar';
+
+const API_KEY = 'AIzaSyAtKGD8idteCC1sjbVVyUN7bfrnU5e83dE';
 
 const App = () => {
-  return  <div>Hi</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
-
 
 ReactDom.render(<App />, document.querySelector('.container'));
